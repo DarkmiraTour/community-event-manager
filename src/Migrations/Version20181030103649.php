2dff449ef8a909e027449f3adb0d7d75b0ba87eb
vincent monjaret <vmonjaret@gmail.com>
2018-11-05 15:32:23 +0100

    Migration is now generic
    Signed-off-by: vincent monjaret <vmonjaret@gmail.com>

--- src/Migrations/Version20181030103649.php
+++ src/Migrations/Version20181030103649.php
@@ -5,24 +5,28 @@ declare(strict_types=1);
 namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Types\Type;
 use Doctrine\Migrations\AbstractMigration;
 
 final class Version20181030103649 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+        $table = $schema->createTable('organisation');
+        $table->addColumn('id', Type::GUID);
+        $table->addColumn('name', Type::STRING, ['length' => 255]);
+        $table->addColumn('website', Type::STRING, ['length' => 255]);
+        $table->addColumn('address', Type::STRING, ['length' => 255, 'notnull' => false]);
+        $table->addColumn('comment', Type::TEXT, ['notnull' => false]);
 
-        $this->addSql('CREATE SEQUENCE organisation_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE organisation (id INT NOT NULL, name VARCHAR(255) NOT NULL, website VARCHAR(255) NOT NULL, address VARCHAR(255) DEFAULT NULL, comment TEXT DEFAULT NULL, PRIMARY KEY(id))');
+        $table->setPrimaryKey(['id']);
+        $table->addUniqueIndex(['id']);
     }
 
     public function down(Schema $schema) : void
     {
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE organisation_id_seq CASCADE');
-        $this->addSql('DROP TABLE organisation');
+        if ($schema->hasTable('organisation')) {
+            $schema->dropTable('organisation');
+        }
     }
 }
