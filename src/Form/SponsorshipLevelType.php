465648b2517b788a932fa8a8c874c17a1f79b3d3
Laurent Bassin <laurent@bassin.info>
2018-11-26 15:03:15 +0100

    Format displayed prices

--- src/Form/SponsorshipLevelType.php
+++ src/Form/SponsorshipLevelType.php
@@ -7,20 +7,28 @@ namespace App\Form;
 use App\Dto\SponsorshipLevelRequest;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\DataMapperInterface;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class SponsorshipLevelType extends AbstractType implements DataMapperInterface
 {
+    private $currency;
+
+    public function __construct(string $currency)
+    {
+        $this->currency = $currency;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('label', TextType::class)
-            ->add('price', NumberType::class)
-            ->setDataMapper($this)
-        ;
+            ->add('price', MoneyType::class, [
+                'currency' => $this->currency,
+            ])
+            ->setDataMapper($this);
     }
 
     public function mapDataToForms($data, $forms): void
