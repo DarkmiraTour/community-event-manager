a26d1b5700b68fd9828f1a138f23cc5d8e512993
Laurent Bassin <laurent@bassin.info>
2018-11-27 11:48:23 +0100

    Add missing import

--- src/Controller/Talk/Show.php
+++ src/Controller/Talk/Show.php
@@ -6,6 +6,7 @@ namespace App\Controller\Talk;
 
 use App\Repository\TalkRepositoryInterface;
 use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Twig\Environment as Twig;
