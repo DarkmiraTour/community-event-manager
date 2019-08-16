3e056ba4e135124a3130082fb63a250a7c38d282
kevin.nadin <kevin.nadin@gmail.com>
2019-08-16 17:50:48 +0200

    add set keyword to permission on minio/events directory

--- docker/minio/create-bucket.sh
+++ docker/minio/create-bucket.sh
@@ -5,4 +5,4 @@ export $(grep -v '^#' .env | xargs)
 /usr/bin/mc config host add minio http://storage:9000 ${S3_KEY} ${S3_SECRET};
 /usr/bin/mc rm -r --force minio/events;
 /usr/bin/mc mb minio/events;
-/usr/bin/mc policy download minio/events;
+/usr/bin/mc policy set download minio/events;
